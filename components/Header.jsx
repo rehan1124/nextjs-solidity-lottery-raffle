e7c9bf594b5796bf439069beb47d58e1fd943558
srehan1 <syed.rehan@shoppertrak.com>
2023-06-20 20:24:30 +0530

    Added connectivity with Metamask

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,55 @@
 import { useMoralis } from "react-moralis";
+import { useEffect } from "react";
 
 const Header = () => {
-  const { enableWeb3 } = useMoralis();
-  return <>I am Header component.</>;
+  const {
+    enableWeb3,
+    account,
+    isWeb3Enabled,
+    Moralis,
+    deactivateWeb3,
+    isWeb3EnableLoading,
+  } = useMoralis();
+
+  useEffect(() => {
+    if (isWeb3Enabled) return;
+    if (window.localStorage.getItem("connected")) {
+      enableWeb3();
+    }
+  }, [isWeb3Enabled]);
+
+  useEffect(() => {
+    Moralis.onAccountChanged((account) => {
+      console.log("Account connected: ", account);
+      if (account == null) {
+        window.localStorage.removeItem("connected");
+        deactivateWeb3();
+        console.log("Account disconnected.");
+      }
+    });
+  });
+
+  // Connect to Metamask
+  const handleButtonClick = async () => {
+    await enableWeb3();
+    window.localStorage.setItem("connected", "injected");
+  };
+
+  return (
+    <>
+      {account ? (
+        <div>Connected to account: {account}!</div>
+      ) : (
+        <button
+          data-testid="mabl-btn-enable-web3"
+          onClick={handleButtonClick}
+          disabled={isWeb3EnableLoading}
+        >
+          Connect!
+        </button>
+      )}
+    </>
+  );
 };
 
 export default Header;
